2434743b6da4de8a7da2cc4feaab9587444e3fdf
Flavio <flaviofz@gmail.com>
2021-11-03 06:26:44 -0300

    Ajuste no retorno padrão da controller main

--- src/ProjetoExemplo.Api/Controllers/MainController.cs
+++ src/ProjetoExemplo.Api/Controllers/MainController.cs
@@ -1,26 +1,44 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using ProjetoExemplo.Business.Interfaces;
 
 namespace ProjetoExemplo.Api.Controllers
 {
+    [ApiController]
     public abstract class MainController : ControllerBase
     {
+        private readonly INotificador _notificador;
+
+        protected MainController(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        protected bool OperacaoValida()
+        {
+            return !_notificador.TemNotificacao();
+        }
+
         protected ActionResult CustomResponse(object result = null)
         {
-            return Ok(
+            if (OperacaoValida())
+            {
+                return Ok(
+                    new
+                    {
+                        success = true,
+                        data = result
+                    }
+                );
+            }
+
+            return BadRequest(
                 new
                 {
-                    success = true,
-                    data = result
+                    success = false,
+                    erros = _notificador.ObterNotificacoes().Select(n => n.Mensagem)
                 }
             );
-
-            // return BadRequest(
-            //     new
-            //     {
-            //         success = false,
-            //         erros = Lista
-            //     }
-            // );
         }
     }
 }
